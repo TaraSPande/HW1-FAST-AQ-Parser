7b7316f1f720b0ded0991e5cc337a406b315a7ba
Tara Pande <tarande57@gmail.com>
2026-01-07 14:19:43 -0800

    coding part and pyproject.toml done

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -10,13 +10,19 @@ def transcribe(seq: str, reverse: bool = False) -> str:
     Write a function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
     """
-    pass
+    transcription = ""
+
+    for ch in seq:
+        transcription.add(TRANSCRIPTION_MAPPING[ch])
+
+    if reverse is False:
+        return transcription
+    else:
+        return transcription[::-1]
 
 def reverse_transcribe(seq: str) -> str:
     """
     Write a function that will transcribe an input sequence and reverse
     the sequence
     """
-    # Hey this is my comment
-    # Again!
-    pass
+    return transcribe(seq, True)
